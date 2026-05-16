fd1d1d99e46c030ba9b9ef70ec15f13892c8ebf9
danielTunmise <danieltumise7@gmail.com>
2026-05-16 09:54:54 +0100

    Added gunicorn, whitenoise, and production settings

--- tradesmart_project/settings.py
+++ tradesmart_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # <-- ADD THIS LINE
 
 # ==========================================
 # EMAIL CONFIGURATION (Gmail SMTP)
