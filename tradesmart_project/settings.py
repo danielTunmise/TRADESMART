9126df1cc79ac32b4e7099d9e6f7ee49e7c6e47e
danielTunmise <danieltumise7@gmail.com>
2026-05-16 13:56:15 +0100

    Switched to console email backend to bypass Render SMTP block

--- tradesmart_project/settings.py
+++ tradesmart_project/settings.py
@@ -124,12 +124,13 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # <-- ADD THIS LINE
 # ==========================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465                  # <-- Change this to 465
+EMAIL_USE_SSL = True              # <-- Add this line
+EMAIL_USE_TLS = False             # <-- Change this to False
 
 # Load sensitive credentials from .env file
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'danieltumise7@gmail.com'
+EMAIL_HOST_PASSWORD = 'dgcaqkwqwkeedzsk'
 
 # Default sender address
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
